a8846b404361f38e395047912dc4c561c858f409
unknown <gokaytemizkan@gmail.com>
2019-12-16 15:43:16 +0300

    attribute changed to string

--- server.py
+++ server.py
@@ -6,17 +6,6 @@ import os
 import sys
 import psycopg2 as dbapi2
 
-import numpy as np
-import matplotlib
-import random
-from threading import Lock
-lock = Lock()
-import mpld3
-from mpld3 import plugins
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-plt.ioff()
-
 app=Flask(__name__)
 
 #DATABASE_URL = os.environ['DATABASE_URL']
@@ -61,43 +50,6 @@ def query(url, scale):
             cursor.close()
             return rows
         
-def draw_fig(fig_type,x,y):
-    """Returns html equivalent of matplotlib figure
-    Parameters
-    ----------
-    fig_type: string, type of figure
-            one of following:
-                    * line
-                    * bar
-    Returns
-    --------
-    d3 representation of figure
-    """
-    #x=np.arange(0,4)
-    pie_fracs = [20, 30, 40, 10]
-    pie_labels = ["A", "B", "C", "D"]
-    
-    with lock:
-        fig, ax = plt.subplots()
-        if fig_type == "line":
-            plt.xlabel("timestamp")
-            plt.ylabel("temprature")
-            plt.ylim(20, 70)
-            ax.plot(x, y)
-        elif fig_type == "bar":
-            ax.bar(x, y)
-        elif fig_type == "pie":
-            ax.pie(pie_fracs, labels=pie_labels)
-        elif fig_type == "scatter":
-            ax.scatter(x, y)
-        elif fig_type == "hist":
-            ax.hist(y, 10, normed=1)
-        elif fig_type == "area":
-            ax.plot(x, y)
-            ax.fill_between(x, 0, y, alpha=0.2)
-
-    return mpld3.fig_to_html(fig,template_type='simple')
-
 @app.route("/")
 def home_page():
     return render_template("home.html")
@@ -125,7 +77,6 @@ def conditions_add_page():
                       INSERT INTO conditions VALUES
                           (%s, '%s', %s, %s); ''' % (form_time, form_location, form_temperature, form_humidity)  ]
         
-        #url="postgres://yhhyfzzbazeqdp:e01c510431281ed149e9aaede179c6a8f02317a6148695c8779bf911d4c8fb9f@ec2-174-129-255-10.compute-1.amazonaws.com:5432/d5esp0qjvi5ic3"
         with dbapi2.connect(DATABASE_URL, sslmode='require') as connection:
             cursor = connection.cursor()
             for statement in STATEMENTS:
@@ -151,14 +102,18 @@ def conditions_remove(time):
 
         return redirect(url_for("conditions_page"))
  
-@app.route('/conditions_plot_<int:attribute>_<string:scale>')
-def conditions_plot_page(attribute, scale):  
+@app.route('/conditions_plot_<string:attribute>_<string:scale>')
+def conditions_plot_page(attribute, scale):
     data = query(DATABASE_URL, scale)
     x=[]
     y=[]
+    if attribute=='temperature':
+        attribute_i=2
+    elif attribute=='humidity':
+        attribute_i=3
     for i in range(0,len(data)):
         x.append(data[i][0])
-        y.append(data[i][attribute])
+        y.append(data[i][attribute_i])
     
     return render_template("conditions_plot.html", x=x, y=y, len_data=len(data))
 
